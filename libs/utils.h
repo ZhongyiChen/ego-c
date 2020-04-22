2c40490896bd1649726879555dc8a5ac23d57f23
BuildTools <unconfigured@null.spigotmc.org>
2020-04-22 18:11:26 +0800

    更换项目目录结构

--- libs/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LIBS_UTILS_H
-#define __LIBS_UTILS_H
-
-void print_arr(int* arr, int len);
-
-void swap(int* a, int* b);
-
-#endif
--- quick-sort/quick_sort_recur.h
+++
@@ -1,6 +0,0 @@
-#ifndef __QUICKSORT_QUICKSORTRECUR_H
-#define __QUICKSORT_QUICKSORTRECUR_H
-
-void quick_sort(int* arr, int len);
-
-#endif
